8be629ecd8a761f142c5ec4c55b8c9c7e75d203a
Ismail Kilicaslan <iskilicaslan61@gmail.com>
2022-01-29 21:43:48 +0100

    aa

--- src/trycommit/TryCommit.java
+++ src/trycommit/TryCommit.java
@@ -3,7 +3,8 @@ package trycommit;
 public class TryCommit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hallo");
 
 	}
 
